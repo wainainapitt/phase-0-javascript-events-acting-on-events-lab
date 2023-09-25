c821af6250448a993603d5325526360a94951e88
peter <pngugi.wainaina@gmail.com>
2023-09-25 11:47:28 +0300

    event lab done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  //moveDodgerRight
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
